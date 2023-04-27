b443efbb94d246dd9224864e4517d426353d954e
NatYarm <yarmnat7@gmail.com>
2023-04-27 21:14:39 +0300

    User login functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -26,7 +27,6 @@ const Register = () => {
     if (isError) {
       toast.error(message);
     }
-
     //Redirect when logged in
     if (isSuccess || user) {
       navigate('/');
@@ -52,6 +52,10 @@ const Register = () => {
       dispatch(register(userData));
     }
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
